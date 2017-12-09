21a6de9e4643710726daf09f87a45fe969e4d65b
Sanyyouisf <sanymamdoh@yahoo.com>
2017-12-08 19:16:38 -0600

    login and register

--- MyOrganizer/App/Controllers/loginController.js
+++ MyOrganizer/App/Controllers/loginController.js
@@ -1,21 +1,35 @@
-﻿app.controller("loginController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
+﻿app.controller("loginController", ["$scope","$rootScope", "$http", "$location", function ($scope,$rootScope, $http, $location) {
+    console.log("inside loginController")
     $scope.username = "";
     $scope.password = "";
-    $rootScope.currentToken = "";
+   // $rootScope.currentToken = "";
     $scope.auth = {};
     $scope.userLogin = {};
     $scope.alerts = [];
     $scope.message = "hi Sany , Login page";
+    //$rootScope.user = user;
+
+    $scope.addAlert = function () {
+        $scope.alerts.push({ msg: 'Another alert!' });
+        console.log("inside addAlert");
+    };
+
+    $scope.closeAlert = function (index) {
+        $scope.alerts.splice(index, 1);
+        console.log("inside closeAlert");
+    };
 
 
     //login function
-    $scope.login = () => {
+    $scope.loginUser = () => {
+        console.log("inside loginUser function");
         $scope.error = "";
+        $scope.alerts = [];
         $scope.inProgress = true;
         $http
             ({
                 method: 'POST',
-                Url: "/Token",
+                url: "/Token",
                     header: { 'Content-Type': 'application/x-www-form-urlencoded' },
                     transformRequest: function (obj) {
                         var str = [];
@@ -23,26 +37,33 @@
                             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
                         return str.join("&");
                     },
-                    data: { grant_type: "password", username: $scope.userLogin.username, password: $scope.userLogin.password }
+                    data: {
+                        grant_type: "password",
+                        username: $scope.userLogin.username,
+                        password: $scope.userLogin.password
+                    }
             })
             .then((result) => {
                 console.log("result in login function :", result);
                 sessionStorage.setItem('token', result.data.access_token);
-                currentToken = sessionStorage.getItem('token');
+                currentToken = sessionStorage.getItem('token');               
                 $http.defaults.headers.common['Authorization'] = `bearer ${result.data.access_token}`;
-                $location.path("/");
                 $scope.inProgress = false;
+                console.log("you loged in as  :", result.data.userName);//the user name which is actually the email.
+                //$scope.alerts.push({ msg: "you loged in as  :" + result.data.userName });// may i need it later to stay for half min 
+                $location.path("/");
             })
             .catch((error) => {
-                //reject(error);
+                //the true message is $scope.error which is error.data.error_description;
                 $scope.error = error.data.error_description;
-                console.log("erro error in login function :", $scope.error);
-                $scope.inProgress = false;
+                $scope.alerts.push({ msg: error.data.error_description });
+                console.log(" error in login function :", $scope.error);
+                $scope.inProgress = false; 
             });
     }
 
     //register function
-    $scope.register = () => {
+    $scope.registerUser = () => {
         var auth = $scope.auth;
         $http({
             method: 'POST',
@@ -50,20 +71,20 @@
             data:
             {
                 UserName: auth.username,
-                Password: auth.password,
-                confirmPassword: auth.confirm,
                 Email: auth.email,
-                FirstName: auth.firstName,
-                LastName: auth.lastName
+                Password: auth.password,
+                confirmPassword: auth.confirm
             }
         })
             .then((resultz) => {
-                resolve(resultz);
                 console.log("resultz in register :", resultz);
+                console.log("regestration Done welcome :", resultz.config.data.UserName);
+                $location.path("/");
             })
             .catch((error) => {
-                reject(error);
-                console.log("error in register :", error)
+                $scope.error = error.data.error_description;
+                $scope.alerts.push({ msg: error.data.Message});
+                console.log("error in register :", error.data.Message);
             });
     };
 }]);
--- MyOrganizer/App/appConfig.js
+++ MyOrganizer/App/appConfig.js
@@ -9,31 +9,41 @@
         {
             templateUrl: "App/Views/login.html",
             controller: "loginController"
-        });
+        })
+        .otherwise('/login');
 }]);
 
 app.run(["$rootScope", "$http", "$location", function ($rootScope, $http, $location) {
     //if a user logged in return the token
     $rootScope.isLoggedIn = function () {
         return !!sessionStorage.getItem("token")
+        console.log("inside isLoggedIn ");
+        //console.log("token :", token );
     }
 
-    $rootScope.$on("$rootChangeStart", function (event, currentRoute) {
-        var anonymousPage = false;
-        var originalPath = currentRoute.originalPath;
+    $rootScope.$on("$routeChangeStart", function (event, currentRoute) {
+        var pageIsAccesibleWithoutBeingLoggedIn = false;
+        //if your not loged in 
+        var pageUserIsNavigatingTo = currentRoute.originalPath;
 
-        //in case
-        if (originalPath) {
-            anonymousPage = originalPath.indexOf("/login") !== -1
+        //don't check this if the page is loading for the first time
+        //if the user is not authenticated 
+        if (pageUserIsNavigatingTo) {
+            pageIsAccesibleWithoutBeingLoggedIn = pageUserIsNavigatingTo === "/login";
+            //console.log("inside pageUserIsNavigatingTo");
         }
 
         //in case the user is not logged in or it is nor anonymous page 
-        if (!anonymousPage && !$rootScope.isLoggedIn()) {
+        if (pageIsAccesibleWithoutBeingLoggedIn || $rootScope.isLoggedIn()) {
+            //let the user through
+           // console.log("the address is not /login ");
+        }
+        else {
             event.preventDefault();
             $location.path("/login");
+            console.log("the user have to loge in ");
         }
 
-        //
         var token = sessionStorage.getItem("token");
         if (token)
             $http.defaults.headers.common["Authorization"] = `bearer ${token}`;
