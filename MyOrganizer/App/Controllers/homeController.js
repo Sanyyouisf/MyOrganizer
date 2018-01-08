4f16ddc4b2b6d011d84d130a4196de54b063a38b
Sanyyouisf <sanymamdoh@yahoo.com>
2018-01-07 23:31:29 -0600

    styling home page

--- MyOrganizer/App/Controllers/homeController.js
+++ MyOrganizer/App/Controllers/homeController.js
@@ -1,21 +1,13 @@
 ﻿app.controller("homeController", ["$scope", "$http","$rootScope", function ($scope, $http, $rootScope) {
     console.log("in homeController");
 
-    $scope.ListToDo = [];
-   // $scope.MeetingListToDo = [];
-    $scope.ToDoList = false;
-
 //------to get the list that not done -----------------------------------------------------
-    $scope.GetListToDo = () => {
         $http.get("api/Tasks/toDoList")
             .then((GetTasksListToDoResult) => {
                 $scope.TasksListToDo = GetTasksListToDoResult.data;
-                console.log("GetTasksListToDoResult data :", $scope.TasksListToDo);
                 $http.get("api/Meetings/toDoList")
                     .then((GetMeetingListToDoResult) => {
                         $scope.MeetingListToDo = GetMeetingListToDoResult.data;
-                        console.log("MeetingListToDo data :", $scope.MeetingListToDo);
-                        $scope.ToDoList = $scope.ToDoList === false ? true: false ;
                     })
                     .catch((error) => {
                         console.log("error in MeetingGetListToDo", error);
@@ -24,7 +16,6 @@
         
             .catch((error) => {
                 console.log("error in TasksGetListToDo", error);
-            })
-    };
+        })
 
 }]);
